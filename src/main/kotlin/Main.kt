63fc2a783a9dae2b2d744a536d2484f3e1b0e30a
viktormih <chronosroseviknr@gmail.com>
2025-03-09 17:46:26 +0300

    First Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 package org.example
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
 fun main() {
-    println("Hello World!")
+
+    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
+
+    val quotes: Elements = doc.select(".sc-2aegk7-2.bzpNIu")
+
+    for (quote : Element in quotes) println("\n${quote.text()}")
 }
